37b3663ca8bf24c39e8a4f800a593a9af9f4cbe3
Haydn Cooke <haydn.r.cooke@gmail.com>
2026-09-02 13:15:30 +0200

    build: include vite wasm dependency

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,9 @@
+import wasm from 'vite-plugin-wasm';
+
 export default {
-  base: './' // CRITICAL: keeps all asset paths relative so it works in a LAMP subdirectory
+  base: './', // CRITICAL: keeps all asset paths relative so it works in a LAMP subdirectory
+  plugins: [wasm()],
+  optimizeDeps: {
+    exclude: ['@dimforge/rapier3d'],
+  },
 };
